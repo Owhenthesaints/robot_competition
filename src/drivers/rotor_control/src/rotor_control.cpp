868afa060a247fb5797cba711b624c05f9be91c1
owhenthesaints <owen.simon@epfl.ch>
2024-03-12 13:00:55 +0100

    made rotor_control_node untested

---
+++ src/drivers/rotor_control/src/rotor_control.cpp
@@ -0,0 +1,62 @@
+#include "rclcpp/rclcpp.hpp"
+#include "example_interfaces/msg/int8.hpp"
+#include <iostream>
+#include <vector>
+
+
+using namespace std::chrono_literals;
+
+class MainControlRotor : public rclcpp::Node
+{
+public:
+    MainControlRotor(int num_children = 2 ) : Node("main_motor_control"), publisher_vector_()
+    {
+        #ifdef VECTOR_INSTRUCTIONS
+        publisher_1_ = this-> create_publisher<example_interfaces::msg::Int8>("motor_updates/0", 10); 
+        publisher_2_ = this-> create_publisher<example_interfaces::msg::Int8>("motor_updates/1", 10);
+        #else
+        for (size_t i =0; i++; i<=num_children){
+            publisher_vector_.push_back(this-> create_publisher<example_interfaces::msg::Int8>("motor_updates/"+std::to_string(i), 10));
+        }
+        #endif
+        timer_ = this-> create_wall_timer(500ms, std::bind(&MainControlRotor::timer_callback, this));
+    }
+private:
+    #ifdef VECTOR_INSTRUCTIONS
+    void timer_callback(){
+        auto wheel1 = example_interfaces::msg::Int8();
+        auto wheel2 = example_interfaces::msg::Int8();
+        // implement vector logic here
+        // placeholder values
+        #ifdef DEBUG
+        wheel1.data = 25 ;
+        wheel2.data = 25;
+        #endif // DEBUG
+        RCLCPP_INFO(this->get_logger(), "Publishing wheel 0: '%s'", wheel1.data.c_str());
+        RCLCPP_INFO(this->get_logger(), "Publishing wheel 1: '%s'", wheel2.data.c_str());
+        publisher_1_-> publish(wheel1);
+        publisher_2_-> publish(wheel2);
+    }
+    #else 
+    void timer_callback(){
+        auto message = example_interfaces::msg::Int8();
+        // to implement
+    }
+    #endif // VECTOR_INSTRUCTIONS
+    rclcpp::TimerBase::SharedPtr timer_;
+    // either two publishers for trigo or one vector of publishers
+    #ifdef VECTOR_INSTRUCTIONS
+    rclcpp::Publisher<example_interfaces::msg::Int8> publisher_1_;
+    rclcpp::Publisher<example_interfaces::msg::Int8> publisher_2_;
+    #else
+    std::vector<std::shared_ptr<rclcpp::Publisher<example_interfaces::msg::Int8>>> publisher_vector_;
+    #endif
+};
+
+
+int main(int argc, char * argv[]){
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<MainControlRotor>());
+    rclcpp::shutdown();
+    return 0;
+}
