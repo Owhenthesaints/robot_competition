8f248cfc6d7e325b0db654c951255ea2ee216d26
Owhenthesaints <owen.simon@epfl.ch>
2024-05-03 01:13:20 +0200

    working untested rx_dispatcher

--- src/drivers/rx_dispatcher/src/rx_dispatcher.cpp
+++ src/drivers/rx_dispatcher/src/rx_dispatcher.cpp
@@ -2,15 +2,16 @@
 #include <functional>
 #include <memory>
 #include <string>
-#include <boost/asio.hpp>
-#include <utility>
-#include <boost/asio.hpp>
+#include <libserial/SerialPort.h>
+#include <vector>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
 
 #define PACKET_LENGTH 10
 #define STOP_CHAR 201
+#define NUM_IMU_VALS 4
+#define NUM_DIST_SENSORS 5
 
 
 using namespace std::chrono_literals;
@@ -18,45 +19,77 @@ using namespace std::chrono_literals;
 template<typename DistSensorPub, typename IMUPub>
 class RxDispatcher : public rclcpp::Node {
 public:
-    explicit RxDispatcher(const std::string &port = "tty/ACM0", int baudRate = 9600) : Node("rx_dispatcher"), io(),
-                                                                                       serial(io, port),
-                                                                                       stored_values(new uint8_t[PACKET_LENGTH-1]) {
+    explicit RxDispatcher(const std::string &port = "tty/ACM0", const int & baudRate = 9600, const size_t & ms_timeout = 10) : Node("rx_dispatcher"),
+                                                                                       stored_values(new uint8_t[PACKET_LENGTH-1]),
+                                                                                       serial(),
+                                                                                       ms_timeout(ms_timeout){
+        // opening serial
+        try {
+            serial.Open(port);
+        } catch(const LibSerial::OpenFailed&){
+            RCLCPP_INFO(this->get_logger(), "failed to open specified file");
+        }
+        // opening publishers
         dist_publisher_ = this->create_publisher<DistSensorPub>("rx/imu/value", 10);
         imu_publisher_ = this->create_publisher<IMUPub>("rx/imu/value", 10);
-        timer_ = this->create_wall_timer(10ms, std::bind(&RxDispatcher::timer_callback, this));
-        serial.set_option(boost::asio::serial_port_base::baud_rate(baudRate));
+        // create timer
+        timer_ = this->create_wall_timer(25ms, std::bind(&RxDispatcher::timer_callback, this));
     }
 
 private:
     void timer_callback() {
-        using namespace boost;
-        auto values = std::make_unique<uint8_t[]>(PACKET_LENGTH);
-        while (true) {
-            asio::read(serial, asio::buffer(&values, PACKET_LENGTH));
-            for (uint8_t i = 0; i < PACKET_LENGTH; i++) {
-                if (values[i] == STOP_CHAR) {
-                    circular_read(values, i);
-                    return;
-                }
+        // this is an std::vector
+        LibSerial::DataBuffer read_buffer;
+        bool found = false;
+        size_t index = 0;
+        try
+        {
+            serial.Read(read_buffer, 0, ms_timeout);
+        }catch (const LibSerial::ReadTimeout &){
+            auto it = read_buffer.begin();
+            while((it = std::find_if(it, [](int val){return val == STOP_CHAR;}))!= read_buffer.end()){
+                found = true;
+                index = std::distance(read_buffer.begin(), it);
+                it++;
             }
         }
+        if (found) {
+            if(index < NUM_IMU_VALS - NUM_DIST_SENSORS) {
+                if(distributeValuesDistance(LibSerial::DataBuffer(read_buffer.begin() + (int)index - NUM_DIST_SENSORS, read_buffer.begin()+(int)index))){
+
+                }
+
+            }
+        }
+
+
+    }
+
+    /**
+     * @brief this function attributes the values to the different sensors
+     * @param distanceValues a subvector containing the distance values
+     * @return true if successful can add more checks
+     */
+    bool distributeValuesDistance(std::vector<uint8_t> distanceValues){
+        if (distanceValues.size() == NUM_DIST_SENSORS){
+            std::copy(distanceValues.begin(), distanceValues.begin() + distSensorArray.size(), distSensorArray.begin());
+            return true;
+        }
+        return false;
     }
 
     /*
      *  A function to reoder the array and store it inside private store_values
      */
-    void circular_read(std::unique_ptr<uint8_t[]> &values, uint8_t begin_point) {
-        for (uint8_t i = begin_point; i!= begin_point - 1; i == PACKET_LENGTH-1 ? i = 0 : i++) {
-            stored_values[i] = values[i];
-        }
-    }
 
     rclcpp::TimerBase::SharedPtr timer_;
     std::shared_ptr<rclcpp::Publisher<DistSensorPub>> dist_publisher_;
     std::shared_ptr<rclcpp::Publisher<IMUPub>> imu_publisher_;
-    boost::asio::io_service io;
-    boost::asio::serial_port serial;
     std::unique_ptr<uint8_t[]> stored_values;
+    LibSerial::SerialPort serial;
+    const size_t ms_timeout;
+    std::array<float, NUM_IMU_VALS> IMUVal;
+    std::array<uint8_t, NUM_DIST_SENSORS> distSensorArray;
 };
 
 int main() {
