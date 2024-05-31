fixed IMU covariance node

dependencies/covariance_calculator.py
@@ -11,13 +11,13 @@ def read_sensor_data():
 
     temperature = mpu6050.get_temp()
 
-    return list(accelerometer_data.values()) + list(gyroscope_data.values()) + [temperature]
+    return list(accelerometer_data.values()) + list(gyroscope_data.values())
 
 if __name__ == "__main__":
-    array = np.empty((0, 7))
+    array = np.empty((0, 6))
     timestamp = time.time()
     while True:
         array = np.vstack([array, read_sensor_data()])
         if time.time() - timestamp > 10:
             break
-    np.savetxt('cov_arr.csv', array, delimiter=',')
+    np.savetxt('imu_vals_10s_arr.csv', array, delimiter=',')

dependencies/covariance_calculator_from_csv.py
@@ -1,7 +1,9 @@
 import numpy as np
 
-data = np.loadtxt('cov_arr.csv', delimiter=',')
+data = np.loadtxt('imu_vals_10s_arr.csv', delimiter=',')
 
 cov_mat = np.cov(data, rowvar=False)
 
 print("Covariance Matrix :\n", cov_mat)
+
+np.savetxt("imu_cov_matrix.csv", cov_mat, delimiter=',')