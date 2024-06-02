78f04c3069df610162da837cd8e4d8f58cfccc59
Owhenthesaints <owen.simon@epfl.ch>
2024-06-02 23:24:50 +0200

    working point package sender to work on a little bit more

--- src/control/legos_detector_py/legos_detector_py/legos_detector_py.py
+++ src/control/legos_detector_py/legos_detector_py/legos_detector_py.py
@@ -1,4 +1,4 @@
-### This code is heavily inspired and some parts are taken from https://github.com/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/tree/master
+# This code is heavily inspired and some parts are taken from https://github.com/EdjeElectronics/TensorFlow-Lite-Object-Detection-on-Android-and-Raspberry-Pi/tree/master
 # Date: 10/29/22
 # This code is subject to the Apache Liscence 2.0
 
@@ -14,17 +14,20 @@ from ament_index_python.packages import get_package_share_directory
 from rclpy.node import Node
 import rclpy
 import importlib.util
+from vision_msgs.msg import Point2D
 
 
 class LegoDetector(Node):
-    
-    def __init__(self, model_name, graph_name, labelmap_name, timer_period = 0.01, use_TPU = False):
+
+    def __init__(self, model_name, graph_name, labelmap_name, min_thresh = 0.5, timer_period=0.1, use_TPU=False):
+        # ROS stuff
         super().__init__("lego_detector")
         self.timer_period = timer_period
         self.timer = self.create_timer(timer_period, self.analyse_vision)
         self.video_capture = cv2.VideoCapture(0)
+        self._publisher = self.create_publisher(Point2D, 'robot/camera/lego_detected', 10)
 
-        ### Vision
+        # Vision
         CWD_PATH = get_package_share_directory('legos_detector_py')
         PATH_TO_CKPT = os.path.join(CWD_PATH, model_name, graph_name)
         PATH_TO_LABELS = os.path.join(CWD_PATH, model_name, labelmap_name)
@@ -44,21 +47,21 @@ class LegoDetector(Node):
         if use_TPU:
             # If user has specified the name of the .tflite file, use that name, otherwise use default 'edgetpu.tflite'
             if (GRAPH_NAME == 'detect.tflite'):
-                GRAPH_NAME = 'edgetpu.tflite'     
+                GRAPH_NAME = 'edgetpu.tflite'
 
         with open(PATH_TO_LABELS, 'r') as f:
             self.labels = [line.strip() for line in f.readlines()]
 
-        self.min_conf_threshold = 0.5
-        #get the working paths 
+        # setup the minimum threshold
+        self.min_conf_threshold = min_thresh
+        # get the working paths
         # define the interpreter
         if use_TPU:
             self.interpreter = Interpreter(model_path=PATH_TO_CKPT,
                                            experimental_delegates=[load_delegate('libedgetpu.so.1.0')])
         else:
             self.interpreter = Interpreter(model_path=PATH_TO_CKPT)
-        
-        
+
         # allocate the tensors
         self.interpreter.allocate_tensors()
 
@@ -66,8 +69,9 @@ class LegoDetector(Node):
         self.__output_details = self.interpreter.get_output_details()
         self.__height = self.__input_details[0]['shape'][1]
         self.__width = self.__input_details[0]['shape'][2]
-        
-        self.__floating_model = (self.__input_details[0]['dtype'] == np.float32)
+
+        self.__floating_model = (
+            self.__input_details[0]['dtype'] == np.float32)
 
         self.__input_mean = 127.5
         self.__input_std = 127.5
@@ -76,31 +80,27 @@ class LegoDetector(Node):
         # because outputs are ordered differently for TF2 and TF1 models
         outname = self.__output_details[0]['name']
 
-        if ('StatefulPartitionedCall' in outname): # This is a TF2 model
+        if ('StatefulPartitionedCall' in outname):  # This is a TF2 model
             self.__boxes_idx, self.__classes_idx, self.__scores_idx = 1, 3, 0
-        else: # This is a TF1 model
+        else:  # This is a TF1 model
             self.__boxes_idx, self.__classes_idx, self.__scores_idx = 0, 1, 2
 
         ret = self.video_capture.set(3, self.__imH)
         ret = self.video_capture.set(4, self.__imW)
         self.frequency = cv2.getTickFrequency()
         self.frame_rate_calc = 1
-    
+
     def release_cap(self):
         self.video_capture.release()
-    
-    def test(self):
-        ret, frame = self.video_capture.read()
-        if ret:
-            cv2.imshow("Beautiful Feed", frame)
-            
-    
 
     def analyse_vision(self):
         t1 = cv2.getTickCount()
-
         # get frame
         hasFrame, frame1 = self.video_capture.read()
+
+        if not hasFrame:
+            self.get_logger.error("cannot get frames")
+            return
 
         # Acquire frame and resize to input shape expected by model [1xHxWx3]
         frame = frame1.copy()
@@ -110,27 +110,33 @@ class LegoDetector(Node):
 
         # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
         if self.__floating_model:
-            input_data = (np.float32(input_data) - self.__input_mean) / self.__input_std
+            input_data = (np.float32(input_data) -
+                          self.__input_mean) / self.__input_std
 
         # Perform detection by running the model with the image as input
-        self.interpreter.set_tensor(self.__input_details[0]['index'],input_data)
+        self.interpreter.set_tensor(
+            self.__input_details[0]['index'], input_data)
         self.interpreter.invoke()
 
-        # Retrieve detection results
-        boxes = self.interpreter.get_tensor(self.__output_details[self.__boxes_idx]['index'])[0] # Bounding box coordinates of detected objects
-        classes = self.interpreter.get_tensor(self.__output_details[self.__classes_idx]['index'])[0] # Class index of detected objects
-        scores = self.interpreter.get_tensor(self.__output_details[self.__scores_idx]['index'])[0] # Confidence of detected objects
+        # Retrieve detection results # Bounding box coordinates of detected objects
+        boxes = self.interpreter.get_tensor(self.__output_details[self.__boxes_idx]['index'])[0] 
+        # Class index of detected objects
+        classes = self.interpreter.get_tensor(self.__output_details[self.__classes_idx]['index'])[0]  
+        # Confidence of detected objects
+        scores = self.interpreter.get_tensor(self.__output_details[self.__scores_idx]['index'])[0]  
 
+        points = np.empty((0, 2))
         # Loop over all detections and process each detection if its confidence is above minimum threshold
         for i in range(len(scores)):
             if ((scores[i] > self.min_conf_threshold) and (scores[i] <= 1.0)):
 
                 # Get bounding box coordinates
                 # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-                ymin = int(max(1,(boxes[i][0] * self.__imH)))
-                xmin = int(max(1,(boxes[i][1] * self.__imW)))
-                ymax = int(min(self.__imH,(boxes[i][2] * self.__imH)))
-                xmax = int(min(self.__imW,(boxes[i][3] * self.__imW)))
+                ymin = int(max(1, (boxes[i][0] * self.__imH)))
+                xmin = int(max(1, (boxes[i][1] * self.__imW)))
+                ymax = int(min(self.__imH, (boxes[i][2] * self.__imH)))
+                xmax = int(min(self.__imW, (boxes[i][3] * self.__imW)))
+                points = np.append(points, np.array([[(xmax-xmin)/2, (ymax - ymin)/2]]), axis = 0)
 
                 # Draw bounding box
                 cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 2)
@@ -142,7 +148,8 @@ class LegoDetector(Node):
                 label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
                 cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
                 cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
-
+        
+        self.get_logger().debug(f"points.shape: {points.shape}")
 
         # Draw framerate in corner of frame
         cv2.putText(frame,'FPS: %.2f' % self.frame_rate_calc,(20,50),cv2.FONT_HERSHEY_PLAIN,2,(0,0,0),4,cv2.LINE_AA)
@@ -152,20 +159,29 @@ class LegoDetector(Node):
         cv2.imshow('Object detector', frame)
         cv2.waitKey(1)
 
+        if points.shape[0] == 0:
+            return
+        
+        distances = np.sum((points - np.array([[self.__imW/2, self.__imH/2]]))**2)
+        msg = Point2D()
+        msg.x = float(points[np.argmin(distances)][0])
+        msg.y = float(points[np.argmin(distances)][1])
+        self._publisher.publish(msg)
+
         # Calculate framerate
         t2 = cv2.getTickCount()
         time1 = (t2-t1)/self.frequency
-        self.frame_rate_calc= 1/time1
+        self.frame_rate_calc= 1/time1 
+
 
 
     def __del__(self):
         self.release_cap()
         cv2.destroyAllWindows()
 
-        
 
-def main(args = None):
-    rclpy.init(args = args)
+def main(args=None):
+    rclpy.init(args=args)
     MODEL_NAME = 'custom_model_lite'
     GRAPH_NAME = 'detect.tflite'
     LABELMAP_NAME = 'labelmap.txt'
