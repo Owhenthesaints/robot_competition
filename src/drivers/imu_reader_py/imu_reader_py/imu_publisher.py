3ea145d34d0c2761cb29dd2d486e16fd968c014f
Owhenthesaints <owen.simon@epfl.ch>
2024-06-03 14:57:38 +0200

    corrected angular + cov values

--- src/drivers/imu_reader_py/imu_reader_py/imu_publisher.py
+++ src/drivers/imu_reader_py/imu_reader_py/imu_publisher.py
@@ -13,12 +13,8 @@ from sensor_msgs.msg import Imu
 class IMUPub(Node):
     def __init__(self):
         super().__init__("IMU_pub")
-        self.__COVARIANCE = np.array([[1.267995719792864911e-03,-4.086623843803083289e-05,2.659803543383608697e-05,6.984727682528767915e-05,-4.621744718599019106e-05,6.864952701665839483e-04],
-                             [-4.086623843803083289e-05,2.219589922458633895e-03,2.535032644819837808e-05,3.952182696245238350e-05,2.985988930803461903e-05,1.622096735774475806e-03],
-                             [2.659803543383608697e-05,2.535032644819837808e-05,2.475723331447657067e-03,-1.238537177369436579e-04,-3.395396012921391836e-05,-9.655998567511241831e-04],
-                             [6.984727682528767915e-05,3.952182696245238350e-05,-1.238537177369436579e-04,9.306212368354672623e-03,-6.645253814496823990e-04,1.678109912451748167e-03],
-                             [-4.621744718599019106e-05,2.985988930803461903e-05,-3.395396012921391836e-05,-6.645253814496823990e-04,1.311478930992902685e-02,1.443720006968440264e-03],
-                             [6.864952701665839483e-04,1.622096735774475806e-03,-9.655998567511241831e-04,1.678109912451748167e-03,1.443720006968440264e-03,6.247741041263324968e-01]])
+        self.__COVARIANCE_GYRO = np.diag([5.4732e-04, 6.1791e-04, 6.2090e-04])
+        self.__COVARIANCE_ACCEL = np.diag([2.8e-03, 2.5e-03, 3.8e-03])
         self.publisher_ = self.create_publisher(Imu, 'robot/imu/value', 10)
         self.mpu = mpu6050.mpu6050(0x68)
         timer_period = 0.07
@@ -36,12 +32,12 @@ class IMUPub(Node):
         imu_msg.linear_acceleration.x = imu_accel['x']
         imu_msg.linear_acceleration.y = imu_accel['y']
         imu_msg.linear_acceleration.z = imu_accel['z']
-        imu_msg.linear_acceleration_covariance = self.__COVARIANCE[0: 3, 0: 3].flatten()
+        imu_msg.linear_acceleration_covariance = self.__COVARIANCE_ACCEL.flatten()
         # setup the gyro data
         imu_msg.angular_velocity.x = imu_giro['x']
         imu_msg.angular_velocity.y = imu_giro['y']
         imu_msg.angular_velocity.z = imu_giro['z']
-        imu_msg.angular_velocity_covariance = self.__COVARIANCE[3:, 3:].flatten()
+        imu_msg.angular_velocity_covariance = self.__COVARIANCE_GYRO.flatten()
 
 
 
