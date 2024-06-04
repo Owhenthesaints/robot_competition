902c910d149a61cfc2943916ea3249bba64554da
Owhenthesaints <owen.simon@epfl.ch>
2024-06-04 15:01:44 +0200

    new launch files

---
+++ launch/motor_control_launch.py
@@ -0,0 +1,39 @@
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+import subprocess
+import time
+
+def open_terminal_and_close():
+    # Command to run in the terminal
+    command = 'timeout 4s pio device monitor;exit;'
+    
+    # Open gnome-terminal and run the command
+    subprocess.Popen(command, shell=True)
+
+
+
+def generate_launch_description():
+    log_level_arg = DeclareLaunchArgument(
+        'log_level',
+        default_value='INFO',
+        description='Log level for the nodes'
+    )
+    open_terminal_and_close()
+
+    time.sleep(2)
+
+    return LaunchDescription([
+        log_level_arg,
+        Node(
+            package='rx_dispatcher',
+            executable='rx_dispatcher',
+            namespace="",
+            name = 'rx_dispatcher',
+            shell =True,
+            arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')]
+        ),
+    ])
+
+
