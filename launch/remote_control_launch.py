58c860ffa69b3e3f7dc68934b032f73e972cfe69
Owhenthesaints <owen.simon@epfl.ch>
2024-06-10 11:55:06 +0200

    remote control_launch calls motor_controll_launch

--- launch/remote_control_launch.py
+++ launch/remote_control_launch.py
@@ -2,37 +2,19 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 import subprocess
 import time
-
-def open_terminal_and_close():
-    # Command to run in the terminal
-    command = 'timeout 4s pio device monitor;exit;'
-    
-    # Open gnome-terminal and run the command
-    subprocess.Popen(command, shell=True)
-
+import os
 
 
 def generate_launch_description():
-    log_level_arg = DeclareLaunchArgument(
-        'log_level',
-        default_value='INFO',
-        description='Log level for the nodes'
-    )
-    open_terminal_and_close()
-
-    time.sleep(2)
+    child_launch_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'motor_control_launch.py')
 
     return LaunchDescription([
-        log_level_arg,
-        Node(
-            package='rx_dispatcher',
-            executable='rx_dispatcher',
-            namespace="",
-            name = 'rx_dispatcher',
-            shell =True,
-            arguments=['--ros-args', '--log-level', LaunchConfiguration('log_level')]
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(child_launch_dir)
         ),
         Node(
             package='joy_override_cpp',
